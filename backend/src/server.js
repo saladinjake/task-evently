7f84e4c73b73343228a6616063c0cdaa74325d1c
saladinjake <juwavicto@gmail.com>
2021-09-28 16:00:00 +0100

    fix: resolve docker backend port exposure and frontend build errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected successfully');
     if (process.env.NODE_ENV !== 'test') {
-      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+      app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
     }
   })
   .catch(err => console.error('MongoDB connection error:', err));
